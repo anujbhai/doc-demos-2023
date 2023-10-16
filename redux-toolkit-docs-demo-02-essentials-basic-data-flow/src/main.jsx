c8d2cc97524271447245b8d79f9cb7cd70cea060
Anuj Upadhyay <anuj.upadhyay@rsystems.com>
2023-10-16 22:51:00 +0530

    redux-toolkit-docs-demo-01-essentials - mocking enabled.

--- redux-toolkit-docs-demo-02-essentials-basic-data-flow/src/main.jsx
+++ redux-toolkit-docs-demo-02-essentials-basic-data-flow/src/main.jsx
@@ -4,9 +4,14 @@ import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
 import store from "./store/index.js";
+import { worker } from "./mocks/browser";
+
+if (import.meta.env.VITE_NODE_ENV === "development") {
+  worker.start();
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
 );
