b7a604402a1b501cf43c301cc2f6e5fc19b41713
Anuj Upadhyay <anuj.upadhyay@rsystems.com>
2023-11-02 23:09:08 +0530

    redux-toolkit-docs-demo-01-essentials - client and server setup done.

--- redux-toolkit-docs-demo-02-essentials-basic-data-flow/src/features/posts/PostPage/index.jsx
+++ redux-toolkit-docs-demo-02-essentials-basic-data-flow/src/features/posts/PostPage/index.jsx
@@ -1,44 +1,13 @@
 import { useSelector } from "react-redux";
-import { NavLink, useParams } from "react-router-dom";
 
-import PostAuthor from "../PostAuthor";
-import TimeAgo from "../TimeAgo";
-import ReactionButtons from "../ReactionButtons";
+import { selectPostById } from "../postsSlice";
+import { useParams } from "react-router-dom";
 
 const PostPage = () => {
   const { postId } = useParams();
 
-  const post = useSelector((state) =>
-    state.posts.find((post) => post.id === postId),
-  );
-
-  if (!post)
-    return (
-      <section>
-        <h2>Post not found</h2>
-      </section>
-    );
-
-  return (
-    <section>
-      <article className="post">
-        <h2>{post.title}</h2>
-
-        <div>
-          <PostAuthor userId={post.user} />
-          <TimeAgo timestamp={post.date} />
-        </div>
-
-        <p className="post-content">{post.content}</p>
-
-        <ReactionButtons post={post} />
-
-        <NavLink to={`/editPosts/${post.id}`} className="button">
-          Edit post
-        </NavLink>
-      </article>
-    </section>
-  );
+  // eslint-disable-next-line no-unused-vars
+  const post = useSelector((state) => selectPostById(state, postId));
 };
 
 export default PostPage;
