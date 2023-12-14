a7d481bb4950e3c18608a8811b9b948896aa7bc0
Anuj Upadhyay <openmindedlearning@gmail.com>
2023-12-14 23:01:18 +0530

    ud-redux-01 - rtk query.

--- redux-code-along-ud/redux-starter-kit-main/src/features/posts/PostsList/index.jsx
+++ redux-code-along-ud/redux-starter-kit-main/src/features/posts/PostsList/index.jsx
@@ -1,4 +1,5 @@
 import React, { memo, useEffect } from 'react'
+import PropTypes from 'prop-types'
 import { useDispatch, useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 
@@ -8,8 +9,8 @@ import ReactionButtons from '../ReactionButtons'
 import Spinner from '../../../components/Spinner'
 import { fetchPosts, selectPostById, selectPostIds } from '../postsSlice'
 
-let PostExcerpt = ({ postId }) => {
-  const post = useSelector(state => selectPostById(state, postId))
+let PostExcerpt = (props) => {
+  const { post } = props
   return (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
@@ -28,7 +29,7 @@ let PostExcerpt = ({ postId }) => {
 
 PostExcerpt = memo(PostExcerpt)
 
-const PostsList = () => {
+function PostsList() {
   const dispatch = useDispatch()
   // const posts = useSelector(selectAllPosts)
   const orderedPosts = useSelector(selectPostIds)
@@ -61,6 +62,16 @@ const PostsList = () => {
       {content}
     </section>
   )
+}
+
+PostsList.propTypes = {
+  post: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    user: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+  }).isRequired,
 }
 
 export default PostsList
